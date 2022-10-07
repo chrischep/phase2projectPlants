9c3f7f5dcb0460a6050ec6631054bf8c08c00d87
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-07 21:43:50 +0300

    style nav list

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,20 +5,21 @@ function NavBar() {
   return (
     <nav className='nav'>
       <h1 className='navh1'>Blossom-Plant Identification</h1>  
-      <div className='navlinks'>
+      <div className='nav-list'>
+       <ul>
+       <NavLink to='/'><li>Home</li>
+       </NavLink>
 
-  <NavLink to='/'>Home
-  </NavLink>
+       <NavLink to='/plantcare'><li>PlantCare</li>
+       </NavLink>
 
-   <NavLink to='/plantcare'>PlantCare
-   </NavLink>
+       <NavLink to='/plantfamily'><li>PlantFamily</li>
+       </NavLink>
 
-   <NavLink to='/plantfamily'>PlantFamily
-   </NavLink>
-
-   <NavLink to='organisation'>Organisation
-   </NavLink>
+       <NavLink to='organisation'><li>Organisation</li>
+       </NavLink>
    
+       </ul>
    </div>
   </nav>
   )
