91c0cc9e5654b09e4a874cea3dd92de6075fa02f
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-07 12:06:58 +0300

    add  data

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const plantcare = [
+    {
+      type: "trees",
+      genres: ["Watering", "Ferterlize periodically", "propagate when needed", "weed overgrown"],
+
+    },
+    {
+        type: "flowers",
+        genres: ["Replenish the water frequently. ", "Trim at least a half inch of stem off your flowers before you plant", "Keep your flowers away from heat and bright light.", "Use sharp scissors when cutting"],
+    },
+    {
+        type: "vegetables",
+        genres: ["Frost protection", "Growth regulators", "Disease and insect control", "Irrigation","Cultivation"],
+    },
+  ];
+  
+  const plantfamily = [
+    {
+      name: "trees",
+      examples: ["cypress family", "cedar family", "red woods family","pine family"],
+    },
+    {
+      name: "flowers",
+      examples: ["mint", "orchid", "iris","myrtle"],
+    },
+    {
+      name: "vegetables",
+      examples: ["POACEAE FAMILY", "THE ROSACEAE FAMILY","THE LAMIACEAE FAMILY","THE UMBELLIFERAE FAMILY"],
+    },
+    
+
+  ];
+  const organisation=[
+    {
+      name: "organisations",
+      examples: ["Kenya - International Fund for Agricultural Development", "Ministry of Agriculture, Livestock, Fisheries and Co-operatives","TruTrade Africa","One Acre Fund","Agriculture and Food Security | Kenya - USAID","Greenlife Crop Protection Africa"],
+    },
+
+    
+
+  ]
+  
+ 
+  export { plantcare,plantfamily,organisation};
+  
